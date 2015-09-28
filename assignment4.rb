edfc137d29fa0c13310b2bc039beff61e2cbb27d
lildre1512 <dcharles@chicagotechacademy.org>
2015-09-28 16:29:38 +0000

    removed extra spaces

--- assignment4.rb
+++ assignment4.rb
@@ -1,8 +1,9 @@
 puts " how many  hours do you work"
-hour = gets. to_i
+hour = gets.to_i
 puts " how much do you make per hour"
-per_hour = gets. to_i
+per_hour = gets.to_i
 puts " how many weeks have you work"
-weeks = gets. to_i
+weeks = gets.to_i
 puts " you would make #{hour*per_hour*weeks}"
+
 
